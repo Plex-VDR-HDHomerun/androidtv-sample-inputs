756b732c729758a6224523bf979a418d7dc65453
Matt Gorski <matt.gorski@gmail.com>
2018-05-14 22:56:47 -0400

    Remove obsolete SyncAdapter and implement new JobService

--- app/src/main/java/com/example/android/sampletvinput/fragment/TvInputSetupFragment.java
+++ app/src/main/java/com/example/android/sampletvinput/fragment/TvInputSetupFragment.java
@@ -1,206 +1,98 @@
 package com.example.android.sampletvinput.fragment;
 
-import android.accounts.Account;
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.SyncStatusObserver;
-import android.media.tv.TvContract;
+import android.content.ComponentName;
 import android.media.tv.TvInputInfo;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v17.leanback.app.BackgroundManager;
-import android.support.v17.leanback.app.BrowseFragment;
-import android.support.v17.leanback.app.DetailsFragment;
-import android.support.v17.leanback.widget.AbstractDetailsDescriptionPresenter;
-import android.support.v17.leanback.widget.Action;
-import android.support.v17.leanback.widget.ArrayObjectAdapter;
-import android.support.v17.leanback.widget.ClassPresenterSelector;
-import android.support.v17.leanback.widget.DetailsOverviewRow;
-import android.support.v17.leanback.widget.DetailsOverviewRowPresenter;
-import android.support.v17.leanback.widget.ListRow;
-import android.support.v17.leanback.widget.ListRowPresenter;
-import android.support.v17.leanback.widget.OnActionClickedListener;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.android.sampletvinput.sync.SyncJobService;
 import com.example.android.sampletvinput.R;
-import com.example.android.sampletvinput.service.AccountService;
-import com.example.android.sampletvinput.player.utils.IptvUtil;
-import com.example.android.sampletvinput.player.utils.SyncUtil;
-import com.example.android.sampletvinput.player.utils.TvContractUtils;
-import com.example.android.sampletvinput.xmltv.XmlTvParser;
+import com.felkertech.settingsmanager.SettingsManager;
+import com.google.android.media.tv.companionlibrary.ChannelSetupFragment;
+import com.google.android.media.tv.companionlibrary.EpgSyncJobService;
 
 /**
- * Fragment which shows a sample UI for registering channels and setting up ChannelSyncAdapter to
+ * Fragment which shows a sample UI for registering channels and setting up SampleJobService to
  * provide program information in the background.
  */
-public class TvInputSetupFragment extends BrowseFragment  {
-    private static final String TAG = TvInputSetupFragment.class.getSimpleName();
-    private static final boolean DEBUG = true;
+public class TvInputSetupFragment extends ChannelSetupFragment {
+    public static final long FULL_SYNC_FREQUENCY_MILLIS = 1000 * 60 * 60 * 24;  // 24 hour
+    private static final long FULL_SYNC_WINDOW_SEC = 1000 * 60 * 60 * 24 * 14;  // 2 weeks
 
-    private static final int ACTION_ADD_CHANNELS = 1;
-    private static final int ACTION_CANCEL = 2;
-    private static final int ACTION_IN_PROGRESS = 3;
-
-    private XmlTvParser.TvListing tvListing = null;
-    private String inputId = null;
-
-    private Action addChannelAction;
-    private Action inProgressAction;
-    private ArrayObjectAdapter adapter;
-    private Object syncObserverHandle;
-    private boolean syncRequested;
+    private String mInputId = null;
+    private int mErrorFound = -1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        inputId = getActivity().getIntent().getStringExtra(TvInputInfo.EXTRA_INPUT_ID);
-        new SetupRowTask().execute();
+        mInputId = getActivity().getIntent().getStringExtra(TvInputInfo.EXTRA_INPUT_ID);
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (syncObserverHandle != null) {
-            ContentResolver.removeStatusChangeListener(syncObserverHandle);
-            syncObserverHandle = null;
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View fragmentView = super.onCreateView(inflater, container, savedInstanceState);
+        setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+        setBadge(getResources().getDrawable(R.mipmap.ic_launcher));
+        setChannelListVisibility(true);
+        setTitle(getString(R.string.app_name));
+        setDescription("");
+        setButtonText(getString(R.string.sync_channels));
+        return fragmentView;
     }
 
-    private class SetupRowTask extends AsyncTask<Uri, String, Boolean> {
+    @Override
+    public void onScanStarted() {
+        EpgSyncJobService.cancelAllSyncRequests(getActivity());
+        SyncJobService.requestImmediateSync1(getActivity(), mInputId, SyncJobService.DEFAULT_IMMEDIATE_EPG_DURATION_MILLIS,
+                new ComponentName(getActivity(), SyncJobService.class));
 
-        @Override
-        protected Boolean doInBackground(Uri... params) {
-            tvListing = IptvUtil.getTvListings(getActivity(),
-                    getString(R.string.iptv_ink_channel_url), IptvUtil.FORMAT_M3U);
-            return tvListing != null;
-        }
+        new SettingsManager(getActivity())
+                .setString(EpgSyncJobService.BUNDLE_KEY_INPUT_ID, mInputId);
 
-        @Override
-        protected void onPostExecute(Boolean success) {
-            if (success) {
-                initUIs();
-            } else {
-                onError(R.string.feed_error_message);
-            }
-        }
-
-        private void initUIs() {
-            DetailsOverviewRowPresenter dorPresenter =
-                    new DetailsOverviewRowPresenter(new DetailsDescriptionPresenter());
-            dorPresenter.setSharedElementEnterTransition(getActivity(), "SetUpFragment");
-
-            addChannelAction = new Action(ACTION_ADD_CHANNELS, getResources().getString(
-                    R.string.tv_input_setup_add_channel));
-            Action cancelAction = new Action(ACTION_CANCEL,
-                    getResources().getString(R.string.tv_input_setup_cancel));
-            inProgressAction = new Action(ACTION_IN_PROGRESS, getResources().getString(
-                    R.string.tv_input_setup_in_progress));
-
-            DetailsOverviewRow row = new DetailsOverviewRow(tvListing);
-            row.addAction(addChannelAction);
-            row.addAction(cancelAction);
-
-            ClassPresenterSelector presenterSelector = new ClassPresenterSelector();
-            // set detail background and style
-            dorPresenter.setBackgroundColor(getResources().getColor(R.color.detail_background));
-            dorPresenter.setStyleLarge(true);
-
-            dorPresenter.setOnActionClickedListener(new OnActionClickedListener() {
-                @Override
-                public void onActionClicked(Action action) {
-                    if (action.getId() == ACTION_ADD_CHANNELS) {
-                        setupChannels(inputId);
-                    } else if (action.getId() == ACTION_CANCEL) {
-                        getActivity().finish();
-                    }
-                }
-            });
-
-            presenterSelector.addClassPresenter(DetailsOverviewRow.class, dorPresenter);
-            presenterSelector.addClassPresenter(ListRow.class, new ListRowPresenter());
-            adapter = new ArrayObjectAdapter(presenterSelector);
-            adapter.add(row);
-
-            setAdapter(adapter);
-
-            BackgroundManager backgroundManager = BackgroundManager.getInstance(getActivity());
-            backgroundManager.attach(getActivity().getWindow());
-            backgroundManager.setDrawable(
-                    getActivity().getDrawable(R.drawable.default_background));
-        }
+        setButtonText(getString(R.string.in_progress));
     }
 
-    private void onError(int errorResId) {
-        Toast.makeText(getActivity(), errorResId, Toast.LENGTH_SHORT).show();
+    @Override
+    public String getInputId() {
+        return mInputId;
+    }
+
+    @Override
+    public void onScanFinished() {
+        if (mErrorFound == -1) {
+            EpgSyncJobService.cancelAllSyncRequests(getActivity());
+            EpgSyncJobService.setUpPeriodicSync(getActivity(), mInputId,
+                    new ComponentName(getActivity(), SyncJobService.class),
+                    FULL_SYNC_FREQUENCY_MILLIS, FULL_SYNC_WINDOW_SEC);
+            getActivity().setResult(Activity.RESULT_OK);
+            Toast.makeText(getActivity(), R.string.toast_scan_channels_added, Toast.LENGTH_SHORT).show();
+        } else {
+            getActivity().setResult(Activity.RESULT_CANCELED);
+            Toast.makeText(getActivity(), R.string.toast_scan_error_found + mErrorFound, Toast.LENGTH_SHORT).show();
+        }
         getActivity().finish();
     }
 
-    private void setupChannels(String inputId) {
-        if (tvListing == null) {
-            onError(R.string.feed_error_message);
-            return;
-        }
-
-        com.example.android.sampletvinput.utils.TvContractUtils.updateChannels(getActivity(), inputId, tvListing.channels);
-        SyncUtil.setUpPeriodicSync(getActivity(), inputId);
-        SyncUtil.requestSync(inputId, true);
-
-        syncRequested = true;
-        // Watch for sync state changes
-        if (syncObserverHandle == null) {
-            final int mask = ContentResolver.SYNC_OBSERVER_TYPE_PENDING |
-                    ContentResolver.SYNC_OBSERVER_TYPE_ACTIVE;
-            syncObserverHandle = ContentResolver.addStatusChangeListener(mask,
-                    mSyncStatusObserver);
+    @Override
+    public void onScanError(int reason) {
+        mErrorFound = reason;
+        switch (reason) {
+            case EpgSyncJobService.ERROR_EPG_SYNC_CANCELED:
+                setDescription(getString(R.string.error_sync_canceled));
+                break;
+            case EpgSyncJobService.ERROR_NO_PROGRAMS:
+            case EpgSyncJobService.ERROR_NO_CHANNELS:
+                setDescription(getString(R.string.error_sync_no_data));
+                break;
+            default:
+                setDescription(getString(R.string.error_sync_default, reason));
+                break;
         }
     }
-
-    private class DetailsDescriptionPresenter extends AbstractDetailsDescriptionPresenter {
-        @Override
-        protected void onBindDescription(ViewHolder viewHolder, Object item) {
-            viewHolder.getTitle().setText(R.string.tv_input_label);
-        }
-    }
-
-    private final SyncStatusObserver mSyncStatusObserver = new SyncStatusObserver() {
-        private boolean syncServiceStarted;
-        private boolean finished;
-
-        @Override
-        public void onStatusChanged(int which) {
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if (finished) {
-                        return;
-                    }
-
-                    Account account = AccountService.getAccount(SyncUtil.ACCOUNT_TYPE);
-                    boolean syncActive = ContentResolver.isSyncActive(account,
-                            TvContract.AUTHORITY);
-                    boolean syncPending = ContentResolver.isSyncPending(account,
-                            TvContract.AUTHORITY);
-                    boolean syncServiceInProgress = syncActive || syncPending;
-                    if (syncRequested && syncServiceStarted && !syncServiceInProgress) {
-                        // Only current programs are registered at this point. Request a full sync.
-                        SyncUtil.requestSync(inputId, false);
-
-                        getActivity().setResult(Activity.RESULT_OK);
-                        getActivity().finish();
-                        finished = true;
-                    }
-                    if (!syncServiceStarted && syncServiceInProgress) {
-                        syncServiceStarted = syncServiceInProgress;
-                        DetailsOverviewRow detailRow = (DetailsOverviewRow) adapter.get(0);
-                        detailRow.removeAction(addChannelAction);
-                        detailRow.addAction(0, inProgressAction);
-                        adapter.notifyArrayItemRangeChanged(0, 1);
-                    }
-                }
-            });
-        }
-    };
-
 }
--- app/src/main/java/com/example/android/sampletvinput/sync/SyncJobService.java
+++ app/src/main/java/com/example/android/sampletvinput/sync/SyncJobService.java
@@ -1,88 +1,275 @@
 package com.example.android.sampletvinput.sync;
 
+import android.app.job.JobInfo;
 import android.app.job.JobParameters;
-import android.app.job.JobService;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
-import android.os.AsyncTask;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.PersistableBundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-import com.example.android.sampletvinput.client.Connection;
-import com.example.android.sampletvinput.setup.SetupUtils;
+import com.example.android.sampletvinput.xmltv.XmlTvParser;
+import com.example.android.sampletvinput.R;
+import com.google.android.media.tv.companionlibrary.EpgSyncJobService;
+import com.google.android.media.tv.companionlibrary.model.Advertisement;
+import com.google.android.media.tv.companionlibrary.model.Channel;
+import com.google.android.media.tv.companionlibrary.model.InternalProviderData;
+import com.google.android.media.tv.companionlibrary.model.Program;
+import com.google.android.media.tv.companionlibrary.utils.TvContractUtils;
 
-public class SyncJobService extends JobService {
+import junit.framework.Assert;
 
-    public static final String TAG = "SyncJobService";
+import org.json.JSONException;
 
-    static class SyncTask extends AsyncTask<Void, Void, Boolean> {
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-        private final ChannelSyncAdapter adapter;
+/**
+ * A periodic task that can be run to synchronize data from Google Drive to the system's internal
+ * TIF database and local cache.
+ */
+public class SyncJobService extends EpgSyncJobService {
+    private static final String TAG = SyncJobService.class.getSimpleName();
+    private static final String TEST_AD_REQUEST_URL =
+            "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/" +
+                    "single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast" +
+                    "&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct" +
+                    "%3Dlinear&correlator=";
+    private static HashMap<String, XmlTvParser.TvListing> epgData;
+    public static final long DEFAULT_IMMEDIATE_EPG_DURATION_MILLIS = 1000 * 60 * 60; // 1 Hour
 
-        SyncTask(ChannelSyncAdapter adapter) {
-            this.adapter = adapter;
+    /**
+     * This method needs to be overridden so that we can do asynchronous actions beforehand.
+     */
+    @Override
+    public boolean onStartJob(final JobParameters params) {
+        // Broadcast status
+        Intent intent = new Intent(ACTION_SYNC_STATUS_CHANGED);
+        intent.putExtra(BUNDLE_KEY_INPUT_ID, params.getExtras().getString(BUNDLE_KEY_INPUT_ID));
+        Log.d(TAG, "Sync program data for " + params.getExtras().getString(BUNDLE_KEY_INPUT_ID));
+        intent.putExtra(SYNC_STATUS, SYNC_STARTED);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+
+        if (AppUtils.isTV(getApplicationContext())) { // Epg syncing only happens on TVs
+            new EpgDataSyncThread(this, new EpgDataCallback() {
+                @Override
+                public void onComplete() {
+                    Log.d(TAG, "Epg data syncing is complete");
+                    Handler h = new Handler(Looper.getMainLooper()) {
+                        @Override
+                        public void handleMessage(Message msg) {
+                            super.handleMessage(msg);
+                            Log.d(TAG, "Run in main thread");
+                            EpgSyncTask epgSyncTask = new EpgSyncTask(params);
+                            epgSyncTask.execute();
+                        }
+                    };
+                    h.sendEmptyMessage(0);
+                }
+            }).start();
+        }
+        return true;
+    }
+
+    @Override
+    public List<Channel> getChannels() {
+        // Build advertisement list for the channel.
+        Advertisement channelAd = new Advertisement.Builder()
+                .setType(Advertisement.TYPE_VAST)
+                .setRequestUrl(TEST_AD_REQUEST_URL)
+                .build();
+        List<Advertisement> channelAdList = new ArrayList<>();
+        channelAdList.add(channelAd);
+
+        InternalProviderData ipd = new InternalProviderData();
+//        ipd.setAds(channelAdList);
+        ipd.setRepeatable(true);
+        ipd.setVideoType(TvContractUtils.SOURCE_TYPE_HLS);
+
+        try {
+            List<Channel> channels = ChannelDatabase.getInstance(this).getChannels(ipd);
+            // Add app linking
+            for (int i = 0; i < channels.size(); i++) {
+                JsonChannel jsonChannel =
+                        ChannelDatabase.getInstance(this).findChannelByMediaUrl(
+                                channels.get(i).getInternalProviderData().getVideoUrl());
+                Channel channel = new Channel.Builder(channels.get(i))
+                        .setAppLinkText(getString(R.string.quick_settings))
+                        .setAppLinkIconUri("https://github.com/Fleker/CumulusTV/blob/master/app/src/m" +
+                                "ain/res/drawable-xhdpi/ic_play_action_normal.png?raw=true")
+                        .setAppLinkPosterArtUri(channels.get(i).getChannelLogo())
+                        .setAppLinkIntent(PlaybackQuickSettingsActivity.getIntent(this, jsonChannel))
+                        .build();
+                Log.d(TAG, "Adding channel " + channel.getDisplayName());
+                channels.set(i, channel);
+            }
+            Log.d(TAG, "Returning with " + channels.size() + " channels");
+            return channels;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.w(TAG, "No channels found");
+        return null;
+    }
+
+    @Override
+    public List<Program> getProgramsForChannel(Uri channelUri, Channel channel, long startMs,
+                                               long endMs) {
+        List<Program> programs = new ArrayList<>();
+        ChannelDatabase channelDatabase = ChannelDatabase.getInstance(this);
+        Log.d(TAG, "Get programs for " + channel.toString());
+        JsonChannel jsonChannel = channelDatabase.findChannelByMediaUrl(
+                channel.getInternalProviderData().getVideoUrl());
+
+        if (jsonChannel != null && jsonChannel.getEpgUrl() != null &&
+                !jsonChannel.getEpgUrl().isEmpty() && epgData.containsKey(jsonChannel.getEpgUrl())) {
+            List<Program> programForGivenTime = new ArrayList<>();
+            LeanbackXmltvParser.TvListing tvListing = epgData.get(jsonChannel.getEpgUrl());
+            if (tvListing == null) {
+                return programs; // Return empty programs.
+            }
+            List<Program> programList = tvListing.getAllPrograms();
+            // If repeat-programs is on, schedule the programs sequentially in a loop. To make
+            // every device play the same program in a given channel and time, we assumes the
+            // loop started from the epoch time.
+            long totalDurationMs = 0;
+            for (Program program : programList) {
+                totalDurationMs += program.getEndTimeUtcMillis() - program.getStartTimeUtcMillis();
+            }
+            long programStartTimeMs = startMs - startMs % totalDurationMs;
+            int i = 0;
+            final int programCount = programList.size();
+            while (programStartTimeMs < endMs) {
+                Program currentProgram = programList.get(i++ % programCount);
+                long programEndTimeMs = currentProgram.getEndTimeUtcMillis();
+                if (programEndTimeMs < startMs) {
+                    programStartTimeMs = programEndTimeMs;
+                    continue;
+                }
+                programForGivenTime.add(new Program.Builder()
+                        .setTitle(currentProgram.getTitle())
+                        .setDescription(currentProgram.getDescription())
+                        .setContentRatings(currentProgram.getContentRatings())
+                        .setCanonicalGenres(currentProgram.getCanonicalGenres())
+                        .setPosterArtUri(currentProgram.getPosterArtUri())
+                        .setThumbnailUri(currentProgram.getThumbnailUri())
+                        .setInternalProviderData(currentProgram.getInternalProviderData())
+                        .setStartTimeUtcMillis(programStartTimeMs)
+                        .setEndTimeUtcMillis(programEndTimeMs)
+                        .build()
+                );
+                programStartTimeMs = programEndTimeMs;
+            }
+            return programForGivenTime;
+        } else {
+            programs.add(new Program.Builder()
+                    .setInternalProviderData(channel.getInternalProviderData())
+                    .setTitle(channel.getDisplayName() + " Live")
+                    .setDescription(getString(R.string.currently_streaming))
+                    .setPosterArtUri(channel.getChannelLogo())
+                    .setThumbnailUri(channel.getChannelLogo())
+                    .setCanonicalGenres(jsonChannel != null ? jsonChannel.getGenres() : null)
+                    .setStartTimeUtcMillis(startMs)
+                    .setEndTimeUtcMillis(startMs + 1000 * 60 * 60) // 60 minutes
+                    .build());
+        }
+        return programs;
+    }
+
+    private final class EpgDataSyncThread extends Thread {
+        private Context mContext;
+        private EpgDataCallback mCallback;
+
+        EpgDataSyncThread(Context context, EpgDataCallback callback) {
+            super();
+            mContext = context;
+            mCallback = callback;
         }
 
         @Override
-        protected Boolean doInBackground(Void... voids) {
-            adapter.reset();
-
-            if(!isCancelled()) {
-                adapter.syncChannels();
+        public void run() {
+            super.run();
+            epgData = new HashMap<>();
+            ChannelDatabase cdn;
+            try {
+                cdn = ChannelDatabase.getInstance(mContext);
+            } catch (ChannelDatabase.MalformedChannelDataException e) {
+                return; // Stop execution now.
             }
-
-            if(!isCancelled()) {
-                adapter.syncEPG();
+            try {
+                List<JsonChannel> channels = cdn.getJsonChannels();
+                for (JsonChannel jsonChannel : channels) {
+                    if (jsonChannel.getEpgUrl() != null && !jsonChannel.getEpgUrl().isEmpty()) {
+                        List<Program> programForGivenTime = new ArrayList<>();
+                        // Load from the EPG url
+                        URLConnection urlConnection = null;
+                        try {
+                            urlConnection = new URL(jsonChannel.getEpgUrl()).openConnection();
+                            urlConnection.setConnectTimeout(1000 * 5);
+                            urlConnection.setReadTimeout(1000 * 5);
+                            InputStream inputStream = urlConnection.getInputStream();
+                            InputStream epgInputStream =  new BufferedInputStream(inputStream);
+                            XmlTvParser.TvListing tvListing = XmlTvParser.parse(epgInputStream);
+                            epgData.put(jsonChannel.getEpgUrl(), tvListing);
+                        } catch (IOException | XmlTvParser.XmlTvParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                mCallback.onComplete();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-
-            if(!isCancelled()) {
-                adapter.syncChannelIcons();
-            }
-
-            return !isCancelled();
-        }
-
-        void cancelJob() {
-            adapter.cancel();
         }
     }
 
-    SyncTask syncTask;
-
-    @Override
-    public boolean onStartJob(final JobParameters jobParameters) {
-        String inputId = SetupUtils.getInputId(this);
-
-        Connection connection = new Connection("roboTV Sync Adapter");
-        connection.setPriority(1);
-
-        Context context = getApplication();
-
-        if(!connection.open(SetupUtils.getServer(context))) {
-            Log.e(TAG, "unable to connect to server");
-            return false;
-        }
-
-        ChannelSyncAdapter syncAdapter = new ChannelSyncAdapter(connection, context, inputId);
-
-        syncTask = new SyncTask(syncAdapter) {
-
-            @Override
-            protected void onPostExecute(Boolean success) {
-                jobFinished(jobParameters, !success);
-            }
-
-        };
-
-        syncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-        return true;
+    private interface EpgDataCallback {
+        void onComplete();
     }
 
-    @Override
-    public boolean onStopJob(JobParameters jobParameters) {
-        if(syncTask != null) {
-            syncTask.cancelJob();
-            syncTask.cancel(true);
+    @Deprecated
+    public static void requestImmediateSync1(Context context, String inputId, long syncDuration,
+                                             ComponentName jobServiceComponent) {
+        if (jobServiceComponent.getClass().isAssignableFrom(EpgSyncJobService.class)) {
+            throw new IllegalArgumentException("This class does not extend EpgSyncJobService");
         }
+        PersistableBundle persistableBundle = new PersistableBundle();
+        if (Build.VERSION.SDK_INT >= 22) {
+            persistableBundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+            persistableBundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        }
+        persistableBundle.putString(EpgSyncJobService.BUNDLE_KEY_INPUT_ID, inputId);
+        persistableBundle.putLong("bundle_key_sync_period", syncDuration);
+        JobInfo.Builder builder = new JobInfo.Builder(1, jobServiceComponent);
+        JobInfo jobInfo = builder
+                .setExtras(persistableBundle)
+                .setOverrideDeadline(1000)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .build();
+        scheduleJob(context, jobInfo);
+        Log.d(TAG, "Single job scheduled");
+    }
 
-        return true;
+    /** Send the job to JobScheduler. */
+    private static void scheduleJob(Context context, JobInfo job) {
+        JobScheduler jobScheduler =
+                (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        int result = jobScheduler.schedule(job);
+        Assert.assertEquals(result, JobScheduler.RESULT_SUCCESS);
+        Log.d(TAG, "Scheduling result is " + result);
     }
 }
